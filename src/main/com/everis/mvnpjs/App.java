e9566e796917489c2099040224f5a381b845fe71
pablo <pjimenezsanchez@safareyes.es>
2021-09-20 13:10:49 +0200

    Primera versión

---
+++ src/main/com/everis/mvnpjs/App.java
@@ -0,0 +1,9 @@
+package com.everis.mvnpjs;
+import org.apache.comons.lang3.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("hello world!"));
+	}
+}
---
+++ src/main/java/com/everis/mvnpjs/App.java
@@ -0,0 +1,14 @@
+package com.everis.mvnpjs;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println(StringUtils.capitalize("hello world!"));
+    }
+}
